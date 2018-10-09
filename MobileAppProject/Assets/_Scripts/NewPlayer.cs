1c1b54094929e84b8670aab14b8c0de9077c9819
Kevin Delassus <g00270791@gmit.ie>
2018-10-09 13:05:52 +0100

    Character Running

--- MobileAppProject/Assets/_Scripts/NewPlayer.cs
+++ MobileAppProject/Assets/_Scripts/NewPlayer.cs
@@ -6,16 +6,26 @@ public class NewPlayer : MonoBehaviour {
 
     private Rigidbody2D myRigidbody;
 
+    private Animator myAnimator;
+
     [SerializeField]
     private float movementSpeed;
 
     private bool faceDirection;
 
+    private bool wave;
+
     // Use this for initialization
     void Start()
     {
         faceDirection = true;
+        myAnimator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        HandleInput();
     }
 
     // Update is called once per frame
@@ -28,11 +38,31 @@ public class NewPlayer : MonoBehaviour {
         HandleMovement(horizontal);
 
         Flip(horizontal);
+
+        HandleWave();
     }
 
     private void HandleMovement(float horizontal)
     {
         myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+
+        myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+    }
+
+    private void HandleWave()
+    {
+        if (wave)
+        {
+            myAnimator.SetTrigger("hi");
+        }
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            wave = true;
+        }
     }
 
     private void Flip(float horizontal)
